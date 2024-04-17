5cc2e501d75c11d5cceb262f0830a0c3b07d92e1
Luciano Gaudio <160156893+SilentShoes@users.noreply.github.com>
2024-04-16 20:49:48 -0400

    ThreeInTheChamber.h

--- ThreeInTheChamber.h
+++ ThreeInTheChamber.h
@@ -1,36 +1,39 @@
-int currentScore = 0;
-int highScore = 0;
-int multiplier = 1;
-int lives = 3;
-int bumperHitBit = 0;
-int drainBit = 0;
-int bumper1Bit = 0;
-int bumper2Bit = 0;
-int bumper3Bit = 0;
-int bumper4Bit = 0;
+bool gameActive = 0;
+unsigned int currentScore = 0;
+unsigned int highScore2 = 0;
+unsigned int multiplier = 0;
+unsigned int lives = 3;
+bool drainBit = 0;
+bool bumperHitBit = 0;
 
-while (gameActive = 1)      //Signal recieved from button
+while (gameActive == 1)      //Signal recieved from button
 
 {
-    if(bumperHitBit = 1)      // Normal bumper is hit
+    if(bumperHitBit == 1)      // Bumper 1 is hit
         {
-            100 * multiplier = currentScore;
+            currentScore = currentScore + (100 * multiplier);
         }
-    
-    if(bumper1Bit = 1,bumper2Bit = 1,bumper3Bit = 1,bumper4Bit)      // Certain bumpers hit increases multiplier
+    if(multiplier == 1)      // Certain bumpers hit increases multiplier
         {
             multiplier++;
         }
-
-    if(drainBit = 1)        //Loss of a life
+    if(drainBit == 1)        //Loss of a life
         {
             lives--;
         }
-    
-    if(lives = 0)       //Out of lives, game over
+    if(lives == 0)       //Out of lives, game over
         {
             gameActive = 0;
         }
 }
 
-currentScore = highScore       //Keep high scores, save to certain variable
+while (gameActive == 0)
+
+{
+    if(currentScore > highScore2)
+        {
+            currentScore = highScore;       //Save high score
+                                            //Push to scoreboard
+            currentScore = 0;              //Reset score
+        }
+}
