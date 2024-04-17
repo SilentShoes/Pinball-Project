0981debe3069bb9bb93961bd9b5e779dbbe59ceb
Luciano Gaudio <160156893+SilentShoes@users.noreply.github.com>
2024-04-16 21:08:38 -0400

    SuddenDeath.h

--- SuddenDeath.h
+++ SuddenDeath.h
@@ -1,6 +1,6 @@
 bool gameActive = 0;
 unsigned int currentScore = 0;
-unsigned int highScore1 = 0;
+unsigned int highScoreSD = 0;
 unsigned int multiplier = 1;
 bool bumperHitBit = 0;
 bool drainBit = 0;
@@ -28,7 +28,7 @@ while (gameActive = 0)
     if(currentScore > highScore1)    //Keep high scores, save to certain variable
     
         {
-            currentScore = highScore;   //Save high score
+            currentScore = highScoreSD;   //Save high score
                                         //Push to scoreboard
             currentScore = 0;           //Reset score
         }
