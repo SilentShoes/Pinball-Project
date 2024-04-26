766cee5d79f9fe037a87fe5578993289ae9f4b2e
Luciano Gaudio <160156893+SilentShoes@users.noreply.github.com>
2024-04-25 21:17:29 -0400

    Create timing.h

---
+++ timing.h
@@ -0,0 +1,26 @@
+#ifndef TIMING_H
+#define TIMING_H
+
+void setCountDown1(unsigned int count);
+unsigned int getCountDown1(void);
+void setCountUp1(unsigned int count);
+unsigned int getCountUp1(void);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
