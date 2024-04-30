e89f0a7134f4b469ba1b1056d09075763ab2bd5d
Luciano Gaudio <160156893+SilentShoes@users.noreply.github.com>
2024-04-30 19:47:17 -0400

    Update and rename ThreeInTheChamber.h to Score.h

--- Score.h
+++ Score.h
@@ -1,6 +1,7 @@
 bool gameActive = 0;
 unsigned int currentScore = 0;
 unsigned int highScoreTITC = 0;
+unsigned int highScoreSD = 0;
 unsigned int multiplier = 0;
 unsigned int lives = 3;
 bool drainBit = 0;
@@ -36,5 +37,35 @@ while (gameActive == 0)
             currentScore = highScoreTITC;       //Save high score
                                             //Push to scoreboard
             currentScore = 0;              //Reset score
+        }
+}
+
+while (gameActive = 1)      //Signal recieved from button
+
+{
+    if(bumperHitBit == 1)       // Normal bumper is hit
+        {
+            currentScore = currentScore + (100 * multiplier);
+            bumperHitBit = 0;
+        }
+    if(10 secs pass)    //Needs interrupt
+        {
+            multiplier++;
+        }
+    if(drainBit == 1)         //Drain hit, game over
+        {
+            gameActive = 0;
+        }
+}
+
+while (gameActive = 0)
+
+{  
+    if(currentScore > highScore1)    //Keep high scores, save to certain variable
+    
+        {
+            currentScore = highScoreSD;   //Save high score
+                                        //Push to scoreboard
+            currentScore = 0;           //Reset score
         }
 }
