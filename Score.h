b4b46badc30ef3e83b1252003810143e72cb5ad0
Luciano Gaudio <160156893+SilentShoes@users.noreply.github.com>
2024-04-30 20:44:36 -0400

    Update Score.h

--- Score.h
+++ Score.h
@@ -1,13 +1,5 @@
-#ifndef SCORE_H
-#define SCORE_H
 
-
-#endif
-
-extern bool gameActive = 0;
-extern unsigned int currentScore = 0;
-extern unsigned int highScoreTITC = 0;
-extern unsigned int highScoreSD = 0;
-extern unsigned int lives = 3;
-
-#endif
+extern gameActive;
+extern currentScore;
+extern highScoreTITC;
+extern lives;
