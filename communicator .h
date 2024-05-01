7cc8e7d00801f56f84c43074dc1f1332ef9cf5a6
KrolAnanas <118073645+KrolAnanas@users.noreply.github.com>
2024-04-30 23:23:54 -0400

    Create communicator .h

---
+++ communicator .h
@@ -0,0 +1,4 @@
+//Public functions
+void MessageReceiver(void);
+void SendMessage(unsigned char dest_address, unsigned char function_flag, unsigned char active_flag, unsigned char lives,
+unsigned char score_byte1, unsigned char score_byte2, unsigned char score_byte3, unsigned char score_byte4)
