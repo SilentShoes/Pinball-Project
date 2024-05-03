ed6fe998689d3a118f22f060632f1c7cefd0f432
KrolAnanas <118073645+KrolAnanas@users.noreply.github.com>
2024-05-02 20:48:57 -0400

    Update Score.c

--- Score.c
+++ Score.c
@@ -23,45 +23,51 @@ unsigned char addressPlunger = 0x07;
 
 char allAddresses[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
 
-while (gameActive == 1)      //Signal recieved from button 
+void scoreCalc( void )
 {
-    //Send gamemode active flag to all
-    for(int i = 0; i < 8; ++i)
+    while (gameActive == 1)      //Signal recieved from button 
     {
-        SendMessage(allAddresses[i], function, gameActive, lives, score); //Figure out score bytes
-    }
-    
-    if(bumperHitBit == 1)      // Bumper 1 is hit
-    {
-        currentScore = currentScore + (100 * multiplier);
-        //Send coontinuous updates to scoreboard
-        SendMessage(addressScore, function, gameActive, lives, score); //Figure out score bytes
-        bumperHitBit = 0;
-    }
-    if(multiplier == 1)      // Certain bumpers hit increases multiplier
-    {
-        multiplier++;
-    }
-    if(drainBit == 1)        //Loss of a life
-    {
-        lives--;
-    }
-    if(lives == 0)       //Out of lives, game over
-    {
-        gameActive = 0; //Turn off game
-
-        // Send (possible) new high score
-        if(currentScore > high_score)
+        //Send gamemode active flag to all
+        for(int i = 0; i < 8; ++i)
         {
-            high_score = currentScore;       //Save high score
-            function = 0x01;    //Change to high score function
-            for(int i = 0; i < 8; ++i)
+            SendMessage(allAddresses[i], function, gameActive, lives, score); //Figure out score bytes
+        }
+        
+        if(bumperHitBit == 1)      // Bumper 1 is hit
+        {
+            currentScore = currentScore + (100 * multiplier);
+            //Send coontinuous updates to scoreboard
+            SendMessage(addressScore, function, gameActive, lives, score); //Figure out score bytes
+            bumperHitBit = 0;
+        }
+        if(multiplier == 1)      // Certain bumpers hit increases multiplier
+        {
+            multiplier++;
+        }
+        if(drainBit == 1)        //Loss of a life
+        {
+            lives--;
+            SendMessage(addressLights, function, gameActive, lives, score); //Figure out score bytes
+            SendMessage(addressMusic, function, gameActive, lives, score); //Figure out score bytes
+        }
+        if(lives == 0)       //Out of lives, game over
+        {
+            gameActive = 0; //Turn off game
+    
+            // Send (possible) new high score
+            if(currentScore > high_score)
             {
-                //Send (possible) new high score & game status to others
-                SendMessage(allAddresses[i], function, gameActive, lives, score); //Figure out score bytes
+                high_score = currentScore;       //Save high score
+                function = 0x01;    //Change to high score function
+                for(int i = 0; i < 8; ++i)
+                {
+                    //Send (possible) new high score & game status to others
+                    SendMessage(allAddresses[i], function, gameActive, lives, score); //Figure out score bytes
+                }
+                function = 0;    //Reset function
+                currentScore = 0;    //Reset score
             }
-            function = 0;    //Reset function
-            currentScore = 0;    //Reset score
         }
     }
+    
 }
