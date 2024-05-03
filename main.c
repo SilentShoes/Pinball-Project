54a50fc85e38669d3ae36cc18db81909bcce6fcf
KrolAnanas <118073645+KrolAnanas@users.noreply.github.com>
2024-05-02 20:49:05 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,13 @@
 #include <msp430g2553.h>
-#include <ThreeInTheChamber.h>
-#include <SuddenDeath.h>
-
-unsigned int gm_three_in_the_chamber = 0;
-unsigned int gm_sudden_death = 0;
+#include <score.h>
+#include <communiciator.h>
 
 void main (void)
 {
-while (1)
-    {
-        if (gm_three_in_the_chamber == 1 && gm_sudden_death == 0)
+    while (1)
         {
-            //Move to ThreeInTheChamber.h
-        } else if (gm_three_in_the_chamber ==1 && gm_sudden_death = 1)
-        {
-            //Move to SuddenDeath.h
-        } else if (gm_three_in_the_chamber ==1 && gm_sudden_death = 1)
-        {
-            gm_three_in_the_chamber = 0;
-            gm_sudden_death = 0;
+            
+            void scoreCalc( void )
+            
         }
     }
-}
